ab04209a77ea6c2be39eeb9efadcf77b8caf8b54
Piyush <piyush41y08h@gmail.com>
2022-02-20 20:18:29 +0530

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,20 @@
+import 'package:auth_flutter/screens/login_screen.dart';
+import 'package:auth_flutter/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
 
 main() {
   runApp(MaterialApp(
+    theme: ThemeData(
+      pageTransitionsTheme: PageTransitionsTheme(builders: {
+        TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+        TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+      }),
+    ),
     title: "Auth",
-    home: App(),
+    initialRoute: "/",
+    routes: {
+      "/": (context) => const LoginScreen(),
+      "/signup": (context) => const SignupScreen(),
+    },
   ));
-}
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 50.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                "Login",
-                style: TextStyle(fontSize: 30),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Email",
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Password",
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text("Login"),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Don't have an account?"),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text("Register"),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                "Login",
+                style: TextStyle(fontSize: 30),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: "Email",
+                ),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: "Password",
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text("Login"),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account?"),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("Register"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
